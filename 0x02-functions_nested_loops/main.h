69efef633ae3c59e1cebf39b6c8fd71ec4825667
yeshwa1 <yeshwaberihun@gmail.com>
2023-03-21 06:28:31 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int c);
+int _abs(int c);
+int _print_last_digit(int c);
+void jack_bauer(void);
+void times_tablet(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_tablet(int n);
