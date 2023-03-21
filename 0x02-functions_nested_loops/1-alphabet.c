244ef9bffc82b629ec6a5d259e3a1b3f7518e948
yeshwa1 <yeshwaberihun@gmail.com>
2023-03-21 06:47:41 -0700

    alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
  * print_alphabet - a function that prints the alphabet, in lowercase
  *
  * Return: Always 0 (Succes)
  */
-void print_alphabet_x10(void);
+void print_alphabet(void)
 {
 	int n;
 
